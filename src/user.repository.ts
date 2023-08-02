7de2fb0228fc3a432f64159488c1819dc7ddd6c9
toannc <canhtoanptit@gmail.com>
2023-08-02 22:36:55 +0700

    update

--- src/user.repository.ts
+++ src/user.repository.ts
@@ -6,6 +6,12 @@ export class UserRepository {
     const arr: number[] = [1, 2, 3];
     const ttn = arr.map((e) => 2 * e).length;
     console.log(ttn);
+    this.doNothing({ x: 10, y: 100 });
     return 'ok';
+  }
+
+  doNothing(pt: { x: number; y: number }) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
   }
 }
