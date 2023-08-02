ef407a28768c40cbff1dd4d0c06b9539a8580cb8
toannc <canhtoanptit@gmail.com>
2023-08-02 22:32:38 +0700

    update

--- src/user.repository.ts
+++ src/user.repository.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UserRepository {
   getUser(): string {
+    const arr: number[] = [1, 2, 3];
+    const ttn = arr.map((e) => 2 * e).length;
+    console.log(ttn);
     return 'ok';
   }
 }
